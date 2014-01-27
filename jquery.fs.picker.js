360b8dbbfc6a230910351883d7bdd9b4e5edaa7e
Ben Plum <mr@benplum.com>
2014-01-27 13:42:43 -0500

    Updated parent to $parent;

--- jquery.fs.picker.js
+++ jquery.fs.picker.js
@@ -161,8 +161,8 @@
 			// EXTEND OPTIONS
 			opts = $.extend({}, opts, $input.data("picker-options"));
 
-			var parent = $input.closest("label"),
-				$label = parent.length ? parent : $("label[for=" + $input.attr("id") + "]"),
+			var $parent = $input.closest("label"),
+				$label = $parent.length ? $parent.eq(0) : $("label[for=" + $input.attr("id") + "]"),
 				type = $input.attr("type"),
 				typeClass = "picker-" + (type === "radio" ? "radio" : "checkbox"),
 				group = $input.attr("name"),
